cc0bac28db7a2a7582208e253224faee758290a9
Rukman <rukmansubedi@gmail.com>
2025-07-31 16:12:32 +0545

    implementation of Navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Logo from '../../assets/logo.png'
 
 const Navbar = () => {
   return (
-    <header>
+    <header className='bg-red-300'>
         <nav>
-            
+            <a href="#" className='flex w-15 h-15 bg-zinc-100 rounded-full'>
+                <img src={Logo} className='w-full h-full object-contain' />
+            </a>
         </nav>
     </header>
   )
