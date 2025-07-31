6e6c54498ad0801b2e5e41f6ec34e585d16d29b6
Rukman <rukmansubedi@gmail.com>
2025-07-31 16:07:34 +0545

    Added assets and updated the favicon and the name of site

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Home from './Components/Home/Home'
 
 const App = () => {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div>
+      <Home/>
+    </div>
   )
 }
 
