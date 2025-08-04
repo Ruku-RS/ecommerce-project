31376b9b50c480fa8a6a7e205329edfed78af009
Rukman <rukmansubedi@gmail.com>
2025-08-04 13:30:47 +0545

    Offer timer set to 5 hours

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import BannerImage from '../../assets/banner.jpg'
 
 const Banner = () => {
+
+    const initialTime = 5 * 60 * 60;
+    const [timeLeft, setTimeLeft] =useState(initialTime);
+
+    const formatTime = (time)=>{
+
+        useEffect(()=>{
+            if(timeLeft <= 0) return
+
+            const timer = setInterval(()=>{
+                setTimeLeft(prev=>{
+                    if(prev <= 1){
+                    clearInterval(timer);
+                    return 0;
+                }
+                    const remainingTime = prev - 1
+                    return remainingTime;
+                })
+
+            }, 1000);
+
+            return ()=> clearInterval(timer);
+
+        }, [timeLeft])
+
+        const hours = Math.floor(time / 3600);
+        const minutes = Math.floor((time % 3600) /60);
+        const seconds = Math.floor(time % 60);
+
+        return{ 
+            hours: String(hours).padStart(2, '0'), 
+            minutes: String(minutes).padStart(2, '0'),
+            seconds: String(seconds).padStart(2, '0')
+        }
+        //padStart (2, '0') means , it'll set two numbers, and if the number is not set , it'll set it to '0'
+    }   
+    const {hours, minutes, seconds} = formatTime(timeLeft);
+
   return (
     <section className='h-[60vh] mt-[14vh] bg-cover bg-top'
      style={{backgroundImage: `url(${BannerImage})`}}>
@@ -13,9 +51,9 @@ const Banner = () => {
                 Up to 50% Off - Limited Time Only!
             </h2>
             <div className='text-6xl font-bold text-zinc-800 flex gap-x-3 mt-5 '>
-                <span className='text-white bg-zinc-800 p-3'>00</span>:
-                <span className='text-white bg-zinc-800 p-3'>00</span>:
-                <span className='text-white bg-zinc-800 p-3'>00</span>
+                <span className='text-white bg-zinc-800 p-3'>{hours}</span>:
+                <span className='text-white bg-zinc-800 p-3'>{minutes}</span>:
+                <span className='text-white bg-zinc-800 p-3'>{seconds}</span>
             </div>
         </div>
     </section>
